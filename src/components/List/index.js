e86267d0b8a3408ab7c7ccec206e8d862bd3d642
Gerard Banasig <tuxrace@yahoo.com>
2021-06-23 22:47:35 +0800

    fix: sorting flatlist

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState, useMemo} from 'react';
 import {FlatList, View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
 import axios from 'axios';
 import strings from '../../strings';
 import {ROOMS_URL} from '../../config';
+import styles from './styles';
 
 const List = props => {
   const {date, time} = props;
@@ -35,14 +37,7 @@ const List = props => {
   const renderItem = ({item}) => {
     return (
       <View
-        style={{
-          padding: 10,
-          backgroundColor: 'lightgrey',
-          margin: 8,
-          justifyContent: 'space-between',
-          flexDirection: 'row',
-          borderRadius: 4,
-        }}>
+        style={styles.renderItemStyle}>
         <View>
           <Text>{item.name}</Text>
           <Text>{`Level ${item.level}`}</Text>
@@ -56,22 +51,28 @@ const List = props => {
   };
 
   const handleSort = () => {
-    setSortAsc(!sortAsc)
+    setSortAsc(!sortAsc);
   };
+
+  const renderData = useMemo(
+    () => () =>
+      data.sort((a, b) => {
+        if (sortAsc) {
+          return parseFloat(a.capacity) - parseFloat(b.capacity);
+        }
+        return parseFloat(b.capacity) - parseFloat(a.capacity);
+      }),
+    [time, sortAsc],
+  );
 
   return (
     <View>
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <Text style={{padding: 8}}>{strings.rooms}</Text>
-        <Button icon="sort" onPress={handleSort} />
+        <Button icon="sort" onPress={handleSort} color={sortAsc ? 'green' : 'blue'} />
       </View>
       <FlatList
-        data={data.sort((a, b) => {
-          if (sortAsc) {
-            return parseFloat(a.capacity) - parseFloat(b.capacity);
-          }
-          return parseFloat(b.capacity) - parseFloat(a.capacity);
-        })}
+        data={renderData()}
         renderItem={renderItem}
         keyExtractor={item => item.name}
       />
---
+++ src/components/List/styles.js
@@ -0,0 +1,10 @@
+export default {
+    renderItemStyle: {
+        padding: 10,
+    backgroundColor: 'lightgrey',
+    margin: 8,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    borderRadius: 4,
+    }
+}
